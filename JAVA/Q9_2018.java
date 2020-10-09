c788e31c879e1f90739f7f7676d4eaff97b2c43b
haneul99 <healing5746@gmail.com>
2020-10-10 01:48:12 +0900

    Q9_2018

--- JAVA/Q9_2018.java
+++ JAVA/Q9_2018.java
@@ -7,46 +7,28 @@ public class Q9_2018 {
 		int n1=input.nextInt();
 		int n2=input.nextInt();
 		int min=Math.min(n1, n2);
-		int tile=2*(n1+n2)-4;
 		for(int i=1;i<=min;i++) {
-			if(i==1) {
-				System.out.printf("1 ");
-				continue;
-			}
-			if(tile%i==0) {
-				
-				if(n1%i==0) {//가로를 다 채운 경우
-					if((n2-2)%i==0) {
-						System.out.printf("%d ",i);
-						continue;
-					}//세로를 다 채운 경우
-					else if((n2-1)%i==0) {
-						if((n1-2)%i==0) {
-							System.out.printf("%d ",i);
-							continue;
-						}
-					}//세로를 한 칸 남긴 경우
-				}
-				
-				else if(n1%i==1) {
-					if((n2-1)%i==0) {
-						System.out.printf("%d ",i);
-						continue;
-					}
-					else if((n2-2)%i==0 && n2%i==0) {
-						System.out.printf("%d ",i);
-						continue;
-					}
-				}
-				
-				else if(n1%i==2) {
-					if(n2%i==0) {
-						System.out.printf("%d ",i);
-						continue;
-					}
-				}
-				
-			}
+			boolean chk=isPossible(n1,n2,i);
+			if(chk==true) System.out.printf("%d ",i);			
 		}
 	}
+	public static boolean isPossible(int nVal, int mVal, int p) {
+		int []n=new int[3];
+		int []m=new int[3];
+		for(int i=0;i<3;i++) {
+			n[i]=(nVal-i)%p;
+			m[i]=(mVal-i)%p;
+		}
+		boolean check=(
+				(n[2]==0 && m[0]==0) ||
+				(n[0]==0 && m[2]==0) || 
+				(n[1]==0 && m[1]==0) ||
+				(n[0]==0 && n[2]==0 && m[1]==0) || 
+				(n[1]==0 && m[0]==0 && m[2]==0))
+				? true : false;
+		//20, 02, 11, 012, 102
+		return check;
+	}
 }
+
+//http://blog.daum.net/rhaoslikesan/182
